dd009fc8609cdae8bd1daf23a0455af11d018002
livingston-lvin <livingstonlvin@gmail>
2021-03-24 18:42:20 +0530

    Added login component and moved login properties from app-component to login-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  formData(f: any){
-    console.log(f.value);
-    f.reset();
-  }
+  
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  formData(f: any) {
+    console.log(f.value);
+    f.reset();
+  }
+
+}
